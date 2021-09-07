e615f2a2a33817d9c3d76ef254fdf342d54e50e4
Gwyn Perry <gmg@users.noreply.github.com>
2021-09-07 11:35:36 +0100

    Fix the Player movement speed
    Added speed field to PlayerController to be able to modifier movement speed in editor.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+    
     private Rigidbody rb;
     private float movementX;
     private float movementY;
@@ -27,6 +29,6 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
     }
 }
