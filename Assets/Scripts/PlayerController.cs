e3dbdb14e3883ca9a154074182cdbb6b748e7c57
Gwyn Perry <gmg@users.noreply.github.com>
2021-09-07 10:08:07 +0100

    Write the OnMove function declaration
    Started implementing movement script. Also fixed Visual Studio config.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,9 +11,8 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue movementValue)
     {
-        
+
     }
 }
