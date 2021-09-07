0b137633079ca55fa989dd5f5ce8116f541da161
Gwyn Perry <gmg@users.noreply.github.com>
2021-09-07 10:52:01 +0100

    Apply force to the Player
    Updated PlayerController script to apply movement input as force to move Player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
     
     // Start is called before the first frame update
     void Start()
@@ -16,10 +18,15 @@ public class PlayerController : MonoBehaviour
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
     void FixedUpdate()
     {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         
+        rb.AddForce(movement);
     }
 }
